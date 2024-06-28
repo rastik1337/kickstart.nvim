fb6f2798b0c0a35a37db9c45226d7c024e8a385c
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2024-06-28 18:10:16 +0200

    vim-fugitive keymap

--- init.lua
+++ init.lua
@@ -84,6 +84,9 @@ end
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Git keymaps
+vim.keymap.set('n', '<leader>g', '<cmd>Git<CR>')
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')

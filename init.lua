144f4b363f9dfe0ceff3ebd87d488a29a9561afe
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2024-07-03 12:16:48 +0200

    list reference and diagnostic quickfix list keymaps

--- init.lua
+++ init.lua
@@ -99,7 +99,7 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>de', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
--- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>dq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -488,8 +488,9 @@ require('lazy').setup({
           --  To jump back, press <C-t>.
           map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
-          -- Find references for the word under your cursor.
+          -- References keymaps
           map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map('<leader>lr', vim.lsp.buf.references, '[L]ist [R]eferences')
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.

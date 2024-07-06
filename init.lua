a004347e171ce0fd746295cf6fbce333eab71782
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2024-07-06 23:47:54 +0200

    better window resizing

--- init.lua
+++ init.lua
@@ -129,6 +129,8 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-w>.', '<C-w>>', { desc = 'Increase window width' })
+vim.keymap.set('n', '<C-w>,', '<C-w><', { desc = 'Decrease window width' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`

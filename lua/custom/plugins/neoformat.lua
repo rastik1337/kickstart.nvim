f3b702f2ae9c49efb9f5061975e5f7b470e3421a
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2024-07-30 20:34:05 +0200

    format only specific filetypes using Neoformat

--- lua/custom/plugins/neoformat.lua
+++ lua/custom/plugins/neoformat.lua
@@ -1,8 +1,25 @@
+local filetypes = {
+  'javascript',
+  'typescript',
+  'python',
+  'php',
+  'vue',
+}
+
+local function format()
+  if vim.tbl_contains(filetypes, vim.bo.filetype) then
+    vim.cmd 'Neoformat'
+    return
+  end
+
+  vim.lsp.buf.format()
+end
+
 return {
   'sbdchd/neoformat',
   config = function()
     vim.g.neoformat_try_node_exe = 1
 
-    vim.keymap.set('n', '<leader>f', ':Neoformat<CR>', { desc = '[F]ormat document' })
+    vim.keymap.set('n', '<leader>f', format, { desc = '[F]ormat document' })
   end,
 }

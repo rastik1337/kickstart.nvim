85009937b0ca26af12b7507cf7fbbd43cf9fbb3c
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2024-09-04 16:30:05 +0200

    added new git keymaps

--- init.lua
+++ init.lua
@@ -389,6 +389,8 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>cb', builtin.git_branches, { desc = '[C]heckout [B]ranch' })
+      vim.keymap.set('n', '<leader>cc', builtin.git_commits, { desc = '[C]heckout [C]ommit' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()

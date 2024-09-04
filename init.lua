eb180b35dfda23ce9429b63b7fbe13653c72602a
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2024-09-04 20:08:01 +0200

    added Comment.nvim

--- init.lua
+++ init.lua
@@ -447,6 +447,8 @@ require('lazy').setup({
       },
     },
   },
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
   { 'Bilal2453/luvit-meta', lazy = true },
   {
     -- Main LSP Configuration

f91b0b2510dd2a56491771f314ad32ea22d87d18
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2024-09-10 02:04:18 +0200

    updated oil.nvim config

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,11 +2,20 @@ return {
   'stevearc/oil.nvim',
   opts = {
     -- default_file_explorer = false,
+    skip_confirm_for_simple_edits = true,
     keymaps = {
       ['<leader>e'] = '<C-w>q',
       ['<C-v>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open in vertical split' },
       ['<C-x>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open in horizontal split' },
       ['<M-p>'] = 'actions.preview',
+    },
+    view_options = {
+      show_hidden = true,
+    },
+    float = {
+      padding = 3,
+      max_width = 80,
+      preview_split = 'below',
     },
   },
   -- Optional dependencies

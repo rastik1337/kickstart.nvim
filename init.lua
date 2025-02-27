078a9ad90ae08ae7aa64b4f71b47df49585b410b
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2025-02-27 21:57:16 +0100

    change treesitter `ensure_installed` to default provided by `kickstart.nvim`

--- init.lua
+++ init.lua
@@ -978,7 +978,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'css', 'vue', 'typescript', 'javascript', 'json', 'php' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {

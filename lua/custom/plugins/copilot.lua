e2eb151de60163f7d4e6e5be88c4908533447e0f
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2025-04-13 00:21:09 +0200

    fix: tab-completion didn't work

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -3,7 +3,7 @@ return {
   init = function()
     vim.g.copilot_enabled = false
 
-    vim.keymap.set('i', '<C-I>', '<Plug>(copilot-suggest)')
+    vim.keymap.set('i', '<C-J>', '<Plug>(copilot-suggest)')
     vim.keymap.set('n', '<leader>cp', ':Copilot<CR>', { desc = '[C]opilot [P]anel' })
   end,
 }

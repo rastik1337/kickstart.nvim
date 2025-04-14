3392f4f9eef75caa3a4a18e79b5f91776e4021a2
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2025-04-15 00:03:00 +0200

    add `code-companion.nvim`

---
+++ lua/custom/plugins/code-companion.lua
@@ -0,0 +1,16 @@
+return {
+  'olimorris/codecompanion.nvim',
+  dependencies = {
+    'j-hui/fidget.nvim',
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  opts = {},
+  init = function()
+    vim.keymap.set({ 'n', 'v' }, '<C-a>', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<leader>a', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true, silent = true })
+    vim.keymap.set('v', 'ga', '<cmd>CodeCompanionChat Add<cr>', { noremap = true, silent = true })
+
+    require('custom.plugins.code-companion.fidget-spinner'):init()
+  end,
+}

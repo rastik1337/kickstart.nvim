b2472dda5246d8dc08bd8c9a4642de42d2ae7bfa
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2025-05-13 09:02:24 +0200

    update copilot keybinds

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -4,6 +4,8 @@ return {
     vim.g.copilot_enabled = false
 
     vim.keymap.set('i', '<C-J>', '<Plug>(copilot-suggest)')
-    vim.keymap.set('n', '<leader>cp', ':Copilot<CR>', { desc = '[C]opilot [P]anel' })
+    vim.keymap.set('n', '<leader>ce', function()
+      vim.g.copilot_enabled = not vim.g.copilot_enabled
+    end, { desc = '[C]opilot Toggl[E]' })
   end,
 }

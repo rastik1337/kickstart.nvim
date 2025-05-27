53f096bfeaf88db9f29a9171622f36d1596fd997
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2025-05-27 11:43:52 +0200

    update lsp config to support Neovim v0.11+

--- init.lua
+++ init.lua
@@ -715,7 +715,7 @@ require('lazy').setup({
           },
           filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'vue' },
         },
-        volar = {},
+        vue_ls = {},
         markdown_oxide = {},
         markdownlint = {},
         tailwindcss = {
@@ -775,19 +775,15 @@ require('lazy').setup({
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
+      ---@type MasonLspconfigSettings
+      ---@diagnostic disable-next-line: missing-fields
       require('mason-lspconfig').setup {
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
+        automatic_enable = vim.tbl_keys(servers or {}),
       }
+
+      for server_name, config in pairs(servers) do
+        vim.lsp.config(server_name, config)
+      end
     end,
   },
 

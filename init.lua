a55683535105a0b0e4c50779b8ff063e66a9e625
rastik1337 <38767414+rastik1337@users.noreply.github.com>
2025-05-28 14:25:47 +0200

    support legacy LSP setup

--- init.lua
+++ init.lua
@@ -781,8 +781,14 @@ require('lazy').setup({
         automatic_enable = vim.tbl_keys(servers or {}),
       }
 
+      local legacy_servers = { 'tailwindcss' }
       for server_name, config in pairs(servers) do
-        vim.lsp.config(server_name, config)
+        if vim.tbl_contains(legacy_servers, server_name) then
+          config.capabilities = vim.tbl_deep_extend('force', capabilities, config.capabilities or {})
+          require('lspconfig')[server_name].setup(config)
+        else
+          vim.lsp.config(server_name, config)
+        end
       end
     end,
   },
